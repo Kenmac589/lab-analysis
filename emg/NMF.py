c7d4006cd1781e0572e7f900b5a0853b7355fb9f
Kenzie MacKinnon <kenziemackinnon5@gmail.com>
2023-04-20 13:41:14 -0300

    Update

--- emg/NMF.py
+++ emg/NMF.py
@@ -1,19 +1,36 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.decomposition import NMF
+from sklearn.decomposition import non_negative_factorization as nnf
 
-testfile = pd.read_excel('test file.xlsx')
-A = np.array(testfile)
+# Load Data
+data = pd.read_csv("normalize-turgay.csv")
+A = data.to_numpy()
 
-W2, H2 = NMF(n_components=2, init='random', random_state=0, max_iter=50).fit_transform(A)
-print(W2)
-print(H2)
-W3, H3 = NMF(n_components=3, init='random', random_state=0, max_iter=50).fit_transform(A)
-W4, H4 = NMF(n_components=4, init='random', random_state=0, max_iter=50).fit_transform(A)
-W5, H5 = NMF(n_components=5, init='random', random_state=0, max_iter=50).fit_transform(A)
-W6, H6 = NMF(n_components=6, init='random', random_state=0, max_iter=50).fit_transform(A)
-W7, H7 = NMF(n_components=7, init='random', random_state=0, max_iter=50).fit_transform(A)
+# Number of components
+nmf = NMF(n_components=2, init='random', random_state=0)
+W2= nmf.fit_transform(A)
+H2 = nmf.components_
+
+nmf = NMF(n_components=3, init='random', random_state=0)
+W3 = nmf.fit_transform(A)
+H3 = nmf.components_
+
+nmf = NMF(n_components=4, init='random', random_state=0)
+W4 = nmf.fit_transform(A)
+H4 = nmf.components_
+
+nmf = NMF(n_components=5, init='random', random_state=0)
+W5 = nmf.fit_transform(A)
+H5 = nmf.components_
+
+nmf = NMF(n_components=6, init='random', random_state=0)
+W6 = nmf.fit_transform(A)
+H6 = nmf.components_
+
+nmf = NMF(n_components=7, init='random', random_state=0)
+W7= nmf.fit_transform(A)
+H7= nmf.components_
 
 C2 = np.dot(W2, H2)
 C3 = np.dot(W3, H3)
@@ -32,4 +49,11 @@ R2All[5] = np.corrcoef(C7.flatten(), A.flatten())[0,1]**2
 
 X = np.array([2, 3, 4, 5, 6, 7])
 plt.plot(X, R2All)
+plt.show()
+
+fig, axs = plt.subplots(num_components, 1, figsize=(6, 3*num_components))
+for i in range(num_components):
+    axs[i].bar(range(len(H_normalized)), H_normalized[:, i])
+    axs[i].set_ylabel('Synergy {}'.format(i+1))
+
 plt.show()
