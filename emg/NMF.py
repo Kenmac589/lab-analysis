3ca75334c8e348c07f31f138609921f6bae248df
Kenzie MacKinnon <kenziemackinnon5@gmail.com>
2023-04-20 13:37:17 -0300

    Update

--- emg/NMF.py
+++ emg/NMF.py
@@ -1,21 +1,35 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.decomposition import NMF
 
-# Load Data
-data = pd.read_csv("normalize-turgay.csv")
-A = data.to_numpy()
+testfile = pd.read_excel('test file.xlsx')
+A = np.array(testfile)
 
-num_components = 7
-nmf = NMF(n_components=num_components, init='random', random_state=0)
-W = nmf.fit_transform(A)
-H = nmf.components_
+W2, H2 = NMF(n_components=2, init='random', random_state=0, max_iter=50).fit_transform(A)
+print(W2)
+print(H2)
+W3, H3 = NMF(n_components=3, init='random', random_state=0, max_iter=50).fit_transform(A)
+W4, H4 = NMF(n_components=4, init='random', random_state=0, max_iter=50).fit_transform(A)
+W5, H5 = NMF(n_components=5, init='random', random_state=0, max_iter=50).fit_transform(A)
+W6, H6 = NMF(n_components=6, init='random', random_state=0, max_iter=50).fit_transform(A)
+W7, H7 = NMF(n_components=7, init='random', random_state=0, max_iter=50).fit_transform(A)
 
-H_normalized = H / np.sum(H, axis=0)
-fig, axs = plt.subplots(num_components, 1, figsize=(6, 3*num_components))
-for i in range(num_components):
-    axs[i].bar(range(len(H_normalized)), H_normalized[:, i])
-    axs[i].set_ylabel('Synergy {}'.format(i+1))
+C2 = np.dot(W2, H2)
+C3 = np.dot(W3, H3)
+C4 = np.dot(W4, H4)
+C5 = np.dot(W5, H5)
+C6 = np.dot(W6, H6)
+C7 = np.dot(W7, H7)
 
+R2All = np.zeros(6)
+R2All[0] = np.corrcoef(C2.flatten(), A.flatten())[0,1]**2
+R2All[1] = np.corrcoef(C3.flatten(), A.flatten())[0,1]**2
+R2All[2] = np.corrcoef(C4.flatten(), A.flatten())[0,1]**2
+R2All[3] = np.corrcoef(C5.flatten(), A.flatten())[0,1]**2
+R2All[4] = np.corrcoef(C6.flatten(), A.flatten())[0,1]**2
+R2All[5] = np.corrcoef(C7.flatten(), A.flatten())[0,1]**2
+
+X = np.array([2, 3, 4, 5, 6, 7])
+plt.plot(X, R2All)
 plt.show()
