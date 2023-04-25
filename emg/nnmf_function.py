084ee2c19fe4b8281ef399f676aa6d04c77d64fb
Kenzie MacKinnon <kenziemackinnon5@gmail.com>
2023-04-25 19:08:36 -0300

    Update

--- emg/nnmf_function.py
+++ emg/nnmf_function.py
@@ -1,43 +1,26 @@
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.decomposition import NMF
 
+def nnmf_factorize(A, k):
+    nmf = NMF(n_components=k, init='random', random_state=0)
+    W = nmf.fit_transform(A)
+    H = nmf.components_
+    C = np.dot(W, H)
+    return W, H, C
+
 # Load Data
 data = pd.read_excel("./test file.xlsx", header=None)
 A = data.to_numpy()
 
-# Number of components
-nmf = NMF(n_components=2, init='random', random_state=0)
-W2= nmf.fit_transform(A)
-H2 = nmf.components_
-
-nmf = NMF(n_components=3, init='random', random_state=0)
-W3 = nmf.fit_transform(A)
-H3 = nmf.components_
-
-nmf = NMF(n_components=4, init='random', random_state=0)
-W4 = nmf.fit_transform(A)
-H4 = nmf.components_
-
-nmf = NMF(n_components=5, init='random', random_state=0)
-W5 = nmf.fit_transform(A)
-H5 = nmf.components_
-
-nmf = NMF(n_components=6, init='random', random_state=0)
-W6 = nmf.fit_transform(A)
-H6 = nmf.components_
-
-nmf = NMF(n_components=7, init='random', random_state=0)
-W7= nmf.fit_transform(A)
-H7= nmf.components_
-
-C2 = np.dot(W2, H2)
-C3 = np.dot(W3, H3)
-C4 = np.dot(W4, H4)
-C5 = np.dot(W5, H5)
-C6 = np.dot(W6, H6)
-C7 = np.dot(W7, H7)
+W2, H2, C2 = nnmf_factorize(A, 2)
+W3, H3, C3 = nnmf_factorize(A, 3)
+W4, H4, C4 = nnmf_factorize(A, 4)
+W5, H5, C5 = nnmf_factorize(A, 5)
+W6, H6, C6 = nnmf_factorize(A, 6)
+W7, H7, C7 = nnmf_factorize(A, 7)
 
 R2All = np.zeros(6)
 R2All[0] = np.corrcoef(C2.flatten(), A.flatten())[0,1]**2
