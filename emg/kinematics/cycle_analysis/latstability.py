a402e9c2ba74fab9108a9dfc2d7147f5a305e5e3
Kenmac589 <kenziemackinnon5@gmail.com>
2024-01-17 13:23:26 -0400

    Update

--- emg/kinematics/cycle_analysis/latstability.py
+++ emg/kinematics/cycle_analysis/latstability.py
@@ -34,6 +34,9 @@ def read_all_csv(directory_path):
 
 def step_duration(input_dataframe):
 
+    """
+    @param: inpo
+    """
     # Define the value and column to search for
     value_to_find = 1
     column_to_search = "45 sw onset"
@@ -79,11 +82,11 @@ def step_duration(input_dataframe):
 
     return adjusted_time_differences, adjusted_treadmill_speeds
 
-def cycle_periods(input_dataframe):
+def extract_cycles(input_dataframe, swonset_channel="44 sw onset"):
 
     # Define the value and column to search for
     value_to_find = 1
-    column_to_search = "45 sw onset"
+    column_to_search = swonset_channel
     column_for_time = "Time"
     column_for_treadmill = "2 Trdml"
 
@@ -107,7 +110,6 @@ def cycle_periods(input_dataframe):
 
     # Finding the average value for the list
     time_differences_array = np.array(time_differences)
-    treadmill_speed_array = np.array(treadmill_speed)
 
     # Creating masks to filter any values above 1 as this would be between distinct recordings
     recording_cutoff_high = 0.6
@@ -117,14 +119,34 @@ def cycle_periods(input_dataframe):
     combined_filter = np.logical_and(cutoff_low, cutoff_high)
 
     # Applying the filter to the arrays
-    adjusted_time_differences = time_differences_array[combined_filter]
-    adjusted_treadmill_speeds = treadmill_speed_array[combined_filter]
-    adj_time_xaxis = np.arange(0, len(adjusted_time_differences))
+    step_cycles = time_differences_array[combined_filter]
 
-    # Finding average step cylce for this length
-    average_step_difference = np.mean(adjusted_time_differences)
+    return step_cycles
 
-    return adjusted_time_differences
+def hip_height(input_dataframe, toey="24 toey", hipy="16 Hipy"):
+
+    # Bringing in the values for toey and hipy
+    toey_values = input_dataframe[toey].tolist()
+    hipy_values = input_dataframe[hipy].tolist()
+    toey_values = np.array(toey_values)
+    hipy_values = np.array(hipy_values)
+
+    # Remove missing values
+    toey_values = toey_values[np.logical_not(np.isnan(toey_values))]
+    hipy_values = hipy_values[np.logical_not(np.isnan(hipy_values))]
+
+    # Converting to purely numerical datatype
+    toey_values = np.array(toey_values, dtype=np.int64)
+    hipy_values = np.array(hipy_values, dtype=np.int64)
+
+    # Getting lower quartile value of toey as proxy for the ground
+    toey_lowerq = np.percentile(toey_values, q=25)
+    average_hip_value = np.mean(hipy_values)
+
+    hip_height = average_hip_value - toey_lowerq
+    return hip_height
+
+# def xcom(input_dataframe):
 
 def cycle_period_summary(directory_path):
 
@@ -157,80 +179,21 @@ def cycle_period_summary(directory_path):
 # Main Code Body
 def main():
 
-    # subject = (
-    #     "WT-M1",
+    wt1nondf = pd.read_csv('./wt_1_non-perturbation.csv', header=0)
+    wt_1_non_step_cycles = extract_cycles(wt1nondf)
 
-    # )
-    data_dict = {}  # Initialize an empty dictionary to store the data
-    data_dict['WT-M1 Non-Perturbation'] = pd.read_csv('./CoM-M1/WT-M1 without Perturbation.txt')
-    data_dict['WT-M1 Perturbation'] = pd.read_csv('./CoM-M1/WT-M1 with Perturbation.txt')
-    data_dict['WT-M2 Non-Perturbation'] = pd.read_csv('./CoM-M2/CoM-M2 without Perturbation.txt')
-    data_dict['WT-M2 Perturbation'] = pd.read_csv('./CoM-M2/CoM-M2 with Perturbation.txt')
-    data_dict['WT-M3 Non-Perturbation'] = pd.read_csv('./CoM-M3/WT-M3 without Peturbation.txt')
-    data_dict['WT-M3 Perturbation'] = pd.read_csv('./CoM-M3/WT-M3 with Perturbation.txt')
-    # data_dict['PreDTX Non-Perturbation'] = pd.read_csv('./M5/PreDTX Without Perturbation.csv')
-    # data_dict['PreDTX Perturbation'] = pd.read_csv('./M5/PreDTX With Perturbation.csv')
-    # data_dict['PostDTX Non-Perturbation'] = pd.read_csv('./M5/PostDTX Without Perturbation.csv')
-    # data_dict['PostDTX Perturbation'] = pd.read_csv('./M5/PostDTX With Perturbation.csv')
+    hipH = hip_height(wt1nondf)
+    print(hipH)
 
-    # Read in all csv's with cycle timing
-    # This is all that really has to change
-    # directory_path = "./M5"
-    trial_list = data_dict
+    wt1perdf = pd.read_csv('./wt_1_perturbation.csv', header=0)
+    wt_1_per_step_cycles = extract_cycles(wt1perdf)
 
-    # cycle_period_summary(directory_path)
-    cycle_results_df = pd.DataFrame()
+    hipH = hip_height(wt1perdf, toey="24 toey (cm)", hipy="16 Hipy (cm)")
+    print(hipH)
 
-    # Initialize Dictionary for storing results for each trial
-    cycle_results = {}
-    for key in trial_list:
-        cycle_results[key] = None
-        cycle_results_df[key] = None
-
-    # Keeping the keys as a list of strings for iteration purposes
-    trials = list(cycle_results.keys())
-
-    # Now, you can access the data from each file like this:
-    for filename, data in trial_list.items():
-        step_duration_array = cycle_periods(data)
-        cycle_results[filename] = step_duration_array
-
-    # Convert Dictionary of Results to Dataframe
-    cycle_results_df = pd.DataFrame(dict([(key, pd.Series(value)) for key, value in cycle_results.items()]))
-
-    cycle_results_df.to_csv("./cycle_comparisons.csv")
-    # pairs = [
-    #     ('WT Non-Perturbation', 'WT Perturbation'),
-    #     ('PreDTX Non-Perturbation', 'WT Non-Perturbation'),
-    #     ('WT Non-Perturbation', 'PostDTX Non-Perturbation'),
-    #     ('PreDTX Perturbation', 'WT Perturbation'),
-    #     ('PreDTX Non-Perturbation', 'PreDTX Perturbation'),
-    #     ('PreDTX Non-Perturbation', 'PostDTX Non-Perturbation'),
-    #     ('PreDTX Non-Perturbation', 'PostDTX Perturbation'),
-    #     ('PreDTX Perturbation', 'PostDTX Non-Perturbation'),
-    #     ('PreDTX Perturbation', 'PostDTX Perturbation'),
-    #     ('PostDTX Non-Perturbation', 'PostDTX Perturbation'),
-    # ]
-
-    non_per = cycle_results_df.loc[:, [col for col in cycle_results_df.columns if 'Non-Perturbation' in col]]
-    # per = cycle_results_df.loc[:, [col for col in cycle_results_df.columns if 'Perturbation' in col]]
-    # Plotting
-    custom_params = {"axes.spines.right": False, "axes.spines.top": False}
-    sns.set(style="white", rc=custom_params)
-
-    plt.title("Step Cycle Durations WT vs DTR M5")
-    plt_cyc = sns.barplot(
-        x=non_per.columns,
-        y=non_per.mean(),
-        order=non_per.columns,
-        zorder=2
-    )
-    plt_cyc.errorbar(x=non_per.columns, y=non_per.mean(), yerr=non_per.std(), capsize=3, fmt="none", c="k", zorder=1)
-    # annotator = Annotator(plt_cyc, pairs, data=cycle_results_df)
-    # annotator.configure(hide_non_significant=True, test='t-test_welch', text_format='simple')
-    # annotator.apply_test().annotate(line_offset_to_group=0.2, line_offset=0.1)
-    # annotator.apply_and_annotate()
-    plt.show()
+    wt4nondf = pd.read_csv('./wt_4_non-perturbation.csv')
+    hipH = hip_height(wt4nondf, toey="24 toey (cm)", hipy="16 Hipy (cm)")
+    print(hipH)
 
 if __name__ == "__main__":
     main()
--- emg/kinematics/cycle_analysis/mos.py
+++ emg/kinematics/cycle_analysis/mos.py
@@ -4,4 +4,8 @@ import pandas as pd
 import seaborn as sns
 import latstability as ls
 
-
+wt1nondf = pd.read_csv('./wt_1_non-perturbation.csv', header=0)
+wt_1_non_step_cycles = ls.extract_cycles(wt1nondf)
+wt1perdf = pd.read_csv('./wt_1_perturbation.csv', header=0)
+wt_1_per_step_cycles = ls.extract_cycles(wt1perdf)
+# np.savetxt(wt_1_step_cycles, './wt_1_non-perturbation_step_cycles.csv', delimiter=',')
