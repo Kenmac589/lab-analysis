7ac342d7789e4f6557c5a27ebc59ec8c17f630bb
Kenmac589 <kenziemackinnon5@gmail.com>
2024-06-20 18:35:50 -0300

    Update

--- kinematics/cycle_analysis/ldir_analysis.py
+++ kinematics/cycle_analysis/ldir_analysis.py
@@ -81,85 +81,152 @@ def median_filter(arr, k):
     return np.array(result)
 
 
+def apply_fir(data, coeff):
+    """applies a simple moving average to your data array.
+    The coefficients define the weights for each sample in the input sequence.
+    In this case, they are [0.5, 0.5], which means that the output is calculated
+    as the weighted average of the last two samples (in this case, `data[n]*coeff[0] + data[n-1]*coeff[1]`).
+    """
+    filtered = signal.lfilter(coeff, 1, data)
+    return filtered
+
+def spike_slope(comy, p):
+    """
+    :param comy: numpy array of the y coordinate of the center of mass
+    :param p: How many 
+
+
+    """
+    data = input_dataframe[comy].values
+
+    n = len(data)
+    slope = [0] * n  # initialize with zeros
+
+    for i in range(p, n - p):
+        past = data[i - p : i]
+        future = data[i + 1 : i + p + 1]
+
+        # calculate means of past and future points
+        mean_past = np.mean(past)
+        mean_future = np.mean(future)
+
+        # update slope at time i using the calculated means
+        slope[i] = (mean_future - mean_past) / 2
+
+    slope = np.array(slope)
+
+    return slope
+
 def cop(fl_y, hl_y):
     return (fl_y + hl_y) / 2
 
 
-# Loading in a dataset
-df, bodyparts, scorer = dlck.load_data(
-    "./lr-walking/ldir/M1_01mps_L_walking_tmDLC_resnet_50_CoM-treadmill_to_leftMar2shuffle1_1030000.h5"
-)
-figure_filename = "./lr-walking/lwalk_M1.png"
-figure_title = "Step Cycle for Video lwalk-M1"
-step_cycles_filename = "./lr-walking/step_cycles-lwalk-M1.csv"
+def main():
 
-# Grabbing toe marker data
-toe = df[scorer]["toe"]
-lhl = df[scorer]["Mirror lHL"]
-rhl = df[scorer]["Mirror rHL"]
-lfl = df[scorer]["Mirror lFL"]
-rfl = df[scorer]["Mirror rFL"]
+    # Settings before running
+    save_results = False
+    figure_title = "Step Cycle for Video lwalk-M1"
+    figure_filename = "./lr-walking/lwalk_M1.png"
+    step_cycles_filename = "./lr-walking/step_cycles-lwalk-M1.csv"
 
-# Converting to numpy array
-toe_np = pd.array(toe["x"])
-rfl_np = pd.array(rfl["y"])
-rhl_np = pd.array(rhl["y"])
-lfl_np = pd.array(lfl["y"])
-lhl_np = pd.array(lhl["y"])
-# com_np = pd.array(com["y"])
+    # Loading in a dataset
+    df, bodyparts, scorer = dlck.load_data(
+        "./lr-walking/ldir/M1_01mps_L_walking_tmDLC_resnet_50_CoM-treadmill_to_leftMar2shuffle1_1030000.h5"
+    )
 
-# Filtering to clean up traces like you would in spike
-toe_filtered = median_filter(toe_np, 9)
-rfl_filtered = median_filter(rfl_np, 9)
-rhl_filtered = median_filter(rhl_np, 9)
-lfl_filtered = median_filter(lfl_np, 9)
-lhl_filtered = median_filter(lhl_np, 9)
+    # Grabbing toe marker data
+    toe = df[scorer]["toe"]
+    lhl = df[scorer]["Mirror lHL"]
+    rhl = df[scorer]["Mirror rHL"]
+    lfl = df[scorer]["Mirror lFL"]
+    rfl = df[scorer]["Mirror rFL"]
+    com = df[scorer]["Mirror CoM"]
 
-# Cleaning up selection to region before mouse moves back
-# toe_roi_selection_fil = toe_filtered[0:2550]
+    # Converting to numpy array
+    toe_np = pd.array(toe["x"])
+    rfl_np = pd.array(rfl["y"])
+    rhl_np = pd.array(rhl["y"])
+    lfl_np = pd.array(lfl["y"])
+    lhl_np = pd.array(lhl["y"])
+    com_np = pd.array(com["y"])
 
-# Center of pressures
-rcop = cop(rfl_filtered, rhl_filtered)
-lcop = cop(lfl_filtered, lhl_filtered)
+    # Filtering to clean up traces like you would in spike
+    toe_med = median_filter(toe_np, 9)
+    rfl_med = median_filter(rfl_np, 9)
+    rhl_med = median_filter(rhl_np, 9)
+    lfl_med = median_filter(lfl_np, 9)
+    lhl_med = median_filter(lhl_np, 9)
+    com_med = median_filter(com_np, 9)
 
-# Calling function for swing estimation
-swing_onset, swing_offset = swing_estimation(toe_filtered)
-step_cyc_durations = step_cycle_est(toe_filtered)
+    # Trying FIR filter
+    coeff = np.array([0.1, 0.2, 0.7])
+    toe_fir = apply_fir(toe_np, coeff)
+    rfl_fir = apply_fir(rfl_np, coeff)
+    rhl_fir = apply_fir(rhl_np, coeff)
+    lfl_fir = apply_fir(lfl_np, coeff)
+    lhl_fir = apply_fir(lhl_np, coeff)
+    com_fir = apply_fir(com_np, coeff)
 
-# Saving values
-np.savetxt(step_cycles_filename, step_cyc_durations, delimiter=",")
+    # Cleaning up selection to region before mouse moves back
+    # toe_roi_selection_fil = toe_filtered[0:2550]
 
-# Calling function for step cycle calculation
+    # Center of pressures
+    rcop = cop(rfl_med, rhl_med)
+    lcop = cop(lfl_med, lhl_med)
 
-# Some of my default plotting parameters I like
-custom_params = {"axes.spines.right": False, "axes.spines.top": False}
-sns.set(style="white", font_scale=1.5, rc=custom_params)
+    # Calling function for swing estimation
+    swing_onset, swing_offset = swing_estimation(toe_med)
+    step_cyc_durations = step_cycle_est(toe_med)
 
-# Plot Legend
-swing_legend = [
-    "Limb X cord",
-    "Swing offset",
-    "Swing onset",
-]
+    # Calling function for step cycle calculation
 
-fig, axs = plt.subplots(2)
-fig.suptitle(figure_title)
+    # Some of my default plotting parameters I like
+    custom_params = {"axes.spines.right": False, "axes.spines.top": False}
+    sns.set(style="white", font_scale=1.5, rc=custom_params)
 
-# For plotting figure demonstrating how swing estimation was done
-axs[0].set_title("Swing Estimation")
-axs[0].plot(toe_filtered)
-axs[0].plot(swing_offset, toe_filtered[swing_offset], "^")
-axs[0].plot(swing_onset, toe_filtered[swing_onset], "v")
-axs[0].legend(swing_legend, loc="best")
+    # Plot Legend
+    swing_legend = [
+        "Limb X cord",
+        "Swing offset",
+        "Swing onset",
+    ]
+    filtest_legend = [
+        "Original",
+        "Median",
+        "FIR",
+    ]
 
-# Showing results for step cycle timing
-axs[1].set_title("xCoM and CoP")
+    fig, axs = plt.subplots(2)
+    fig.suptitle(figure_title)
 
-axs[1].bar(0, np.mean(step_cyc_durations), yerr=np.std(step_cyc_durations), capsize=5)
+    # Showing results for step cycle timing
+    axs[0].set_title("Filter test")
+    # axs[0].plot(com_np)
+    axs[0].plot(com_med)
+    # axs[0].plot(com_fir)
+    # axs[0].legend(filtest_legend, loc="best")
+    # axs[0].bar(0, np.mean(step_cyc_durations), yerr=np.std(step_cyc_durations), capsize=5)
+
+    # For plotting figure demonstrating how swing estimation was done
+    axs[1].set_title("Swing Estimation")
+    axs[1].plot(toe_med)
+    axs[1].plot(swing_offset, toe_med[swing_offset], "^")
+    axs[1].plot(swing_onset, toe_med[swing_onset], "v")
+    axs[1].legend(swing_legend, loc="best")
+
+    # Saving Figure in same folder
+    fig = mpl.pyplot.gcf()
+    fig.set_size_inches(19.8, 10.80)
+    plt.show()
+
+    if save_results is True:
+        # Saving plot and results
+        np.savetxt(step_cycles_filename, step_cyc_durations, delimiter=",")
+        plt.savefig(figure_filename, dpi=300)
+        print("Results saved")
+    else:
+        print("Results not saved")
 
 
-# Saving Figure in same folder
-fig = mpl.pyplot.gcf()
-fig.set_size_inches(19.8, 10.80)
-plt.show()
-plt.savefig(figure_filename, dpi=300)
+if __name__ == "__main__":
+    main()
